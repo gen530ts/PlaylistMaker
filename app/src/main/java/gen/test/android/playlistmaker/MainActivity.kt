a7dd6b1ab35ab395803c5aca0b0006c53fbc151a
gen <gen@home530>
2023-12-19 22:16:38 +0300

    homeWork2_3

--- app/src/main/java/gen/test/android/playlistmaker/MainActivity.kt
+++ app/src/main/java/gen/test/android/playlistmaker/MainActivity.kt
@@ -1,11 +1,36 @@
 package gen.test.android.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn1 = findViewById<Button>(R.id.button1)
+
+        val btn1ClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку1!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn1.setOnClickListener(btn1ClickListener)
+        val btn2 = findViewById<Button>(R.id.button2)
+
+        btn2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку2!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        val btn3 = findViewById<Button>(R.id.button3)
+        btn3.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку3!", Toast
+                .LENGTH_SHORT)
+                .show()
+        }
+
     }
 }
