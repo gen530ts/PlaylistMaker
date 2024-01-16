e5c24412e1aef56c92ad9253451cfb264d24a8aa
gen <gen@home530>
2024-01-16 23:45:26 +0300

    main

--- app/src/main/java/gen/test/android/playlistmaker/SearchActivity.kt
+++ app/src/main/java/gen/test/android/playlistmaker/SearchActivity.kt
@@ -1,11 +1,72 @@
 package gen.test.android.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    private var savedState=""
+    private lateinit var searchEdit:EditText
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun setListeners() {
+        val clear = findViewById<ImageView>(R.id.clearImageView)
+        searchEdit = findViewById(R.id.searchEditText)
+
+        clear.visibility = View.GONE
+        clear.setOnClickListener {
+            searchEdit.text.clear()
+            val imm: InputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(window.currentFocus!!.windowToken, 0)
+
+            Log.d("MyTag", "clearClick")
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clear.visibility = clearButtonVisibility(s)
+                savedState=s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchEdit.addTextChangedListener(simpleTextWatcher)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        setListeners()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("info",savedState)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedState= savedInstanceState.getString("info").toString()
+        searchEdit.setText(savedState)
     }
 }
--- app/src/main/java/gen/test/android/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/gen/test/android/playlistmaker/SettingsActivity.kt
@@ -1,21 +1,91 @@
 package gen.test.android.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
-    private fun setBackListener(){
-        val backImg=findViewById<ImageView>(R.id.backImageView)
+    private var chooserTitle:String = ""
+
+    private fun setBackListener() {
+        val backImg = findViewById<ImageView>(R.id.backImageView)
         backImg.setOnClickListener {
             finish()
-            /*val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)*/
         }
     }
+
+    private fun setShareListener() {
+        val share = findViewById<TextView>(R.id.shareTextView)
+        share.setOnClickListener {
+            val txtMessage = resources.getString(R.string.txt_Message)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, txtMessage)
+            shareIntent.type="text/plain"
+           // shareIntent.putExtra(Intent.EXTRA_COMPONENT_NAME,
+           // shareIntent.putExtra(Intent.EXTRA_PACKAGE_NAME,"com.android.messaging")
+            //shareIntent.putExtra(Intent.EXTRA_PACKAGE_NAME
+           // shareIntent.`package`="com.android.messaging"
+            startActivity(Intent.createChooser(shareIntent, chooserTitle))
+        }
+    }
+
+    private fun setSupportListener() {
+        val support = findViewById<TextView>(R.id.supportTextView)
+        support.setOnClickListener {
+            val  uri: Uri =Uri.parse("mailto:")
+            val shareIntent = Intent(Intent.ACTION_SENDTO,uri)
+           // shareIntent.putExtra(Intent.EXTRA_, resources.getString(R.string.nameEmail))
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.nameEmail)) )
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT,  resources.getString(R.string.subjectEmail))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.txtEmail) )
+            //shareIntent.type = "text/plain"
+            startActivity(Intent.createChooser(shareIntent, chooserTitle))
+        }
+    }
+    private fun setAgreeListener() {
+        val agree = findViewById<TextView>(R.id.agreeTextView)
+        agree.setOnClickListener {
+          val  uri: Uri =Uri.parse(resources.getString(R.string.linkOferta))
+            val agreeIntent = Intent(Intent.ACTION_VIEW,uri)
+            startActivity(Intent.createChooser(agreeIntent, chooserTitle))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        chooserTitle = resources.getString(R.string.chooser_Title)
         setBackListener()
+        setShareListener()
+        setSupportListener()
+        setAgreeListener()
     }
 }
+
+//val txtMessage = resources.getString(R.string.txt_Message)
+//shareIntent.putExtra(Intent.EXTRA_TEXT, txtMessage)
+//shareIntent.type = "text/plain"
+//val message = "Привет, Android-разработка — это круто!"
+// val myUri: Uri = Uri.parse("www.google.com")
+// "<p>Visit <a href=\"http://www.google"+
+//  ".com\">google</a> for more info.</p>")
+//shareIntent.putExtra(Intent.EXTRA_HTML_TEXT
+// shareIntent.putExtra(Intent.EXTRA_TEXT, "hi, $myUri")
+// val uri:URI =URI.pa
+//shareIntent.type = "text/plain"
+//shareIntent.component=
+//shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+//  +
+//    ")
+//shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+/*val sendIntent: Intent = Intent().apply {
+action = Intent.ACTION_SEND
+putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+type = "text/plain"
+}*/
+
+
