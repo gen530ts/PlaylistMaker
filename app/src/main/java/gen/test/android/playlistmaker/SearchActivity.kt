aea090e4235c5999665407627a3c4fd0c37bbc5a
gen <gen@home530>
2024-01-31 22:24:55 +0300

    commit 2

--- app/src/main/java/gen/test/android/playlistmaker/SearchActivity.kt
+++ app/src/main/java/gen/test/android/playlistmaker/SearchActivity.kt
@@ -25,8 +25,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
     private fun initArr(): ArrayList<Track> {
-        return arrayListOf(Track("Smells Like Teen Spirit" +
-                "Teen Spirit",
+        return arrayListOf(Track("Smells Like Teen Spirit",
             "Nirvana",
             "5:01",
             "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
